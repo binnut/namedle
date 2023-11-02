b608982789206d813b93abede356e780214a3fcb
Albin Furin <albin.furin@aspire.se>
2023-11-02 17:14:31 +0100

    fix: Update How to Play

--- src/components/widgets/Tutorial.svelte
+++ src/components/widgets/Tutorial.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <h3>how to play</h3>
-<div>Guess the <strong>WORDLE</strong> in {ROWS} tries.</div>
-<div>Each guess must be a valid {COLS} letter word. Hit the enter button to submit.</div>
+<div>Guess the <s>WORDLE</s><strong>NAME</strong> in {ROWS} tries.</div>
+<div>Each guess must be a valid {COLS} letter name or shorter. In case of a shorter name, fill in with "*" at the end. Hit the enter button to submit.</div>
 <div>
 	After each guess, the color of the tiles will change to show how close your guess was to the
 	word.
@@ -15,32 +15,32 @@
 <div class:complete={visible} class="examples">
 	<div><strong>Examples</strong></div>
 	<div class="row">
-		<Tile value="w" state="🟩" />
-		<Tile value="e" state="🔳" />
+		<Tile value="a" state="🟩" />
+		<Tile value="n" state="🔳" />
+		<Tile value="n" state="🔳" />
 		<Tile value="a" state="🔳" />
-		<Tile value="r" state="🔳" />
-		<Tile value="y" state="🔳" />
+		<Tile value="*" state="🔳" />
 	</div>
-	<div>The letter <strong>W</strong> is in the word and in the correct spot.</div>
+	<div>The letter <strong>E</strong> is in the name and in the correct spot.</div>
 	<div class="row">
-		<Tile value="p" state="🔳" />
-		<Tile value="i" state="🟨" />
+		<Tile value="a" state="🔳" />
+		<Tile value="l" state="🟨" />
+		<Tile value="b" state="🔳" />
+		<Tile value="i" state="🔳" />
+		<Tile value="n" state="🔳" />
+	</div>
+	<div>The letter <strong>L</strong> is in the name but in the wrong spot.</div>
+	<div class="row">
+		<Tile value="e" state="🔳" />
 		<Tile value="l" state="🔳" />
-		<Tile value="l" state="🔳" />
+		<Tile value="i" state="🔳" />
+		<Tile value="a" state="⬛" />
 		<Tile value="s" state="🔳" />
 	</div>
-	<div>The letter <strong>I</strong> is in the word but in the wrong spot.</div>
-	<div class="row">
-		<Tile value="v" state="🔳" />
-		<Tile value="a" state="🔳" />
-		<Tile value="g" state="🔳" />
-		<Tile value="u" state="⬛" />
-		<Tile value="e" state="🔳" />
-	</div>
-	<div>The letter <strong>U</strong> is not in the word in any spot.</div>
+	<div>The letter <strong>A</strong> is not in the name in any spot.</div>
 </div>
 <div>
-	This is a recreation of the original <a
+	This is based on a recreation of the original <a
 		href="https://www.nytimes.com/games/wordle/"
 		target="_blank"
 		rel="noreferrer">Wordle</a
@@ -50,8 +50,11 @@
 	<br /><br />
 	Open the settings menu to see some of the additional features.
 	<br />
-	Written with Svelte, in Typescript by
+	Based on version Written with Svelte, in Typescript by
 	<a href="https://github.com/MikhaD" target="_blank" rel="noreferrer">MikhaD</a>.
+	Edited for use as baby name reveal by 
+	<a href="https://github.com/binnut" target="_blank" rel="noreferrer">Binnut</a>.
+
 </div>
 
 <style lang="scss">
